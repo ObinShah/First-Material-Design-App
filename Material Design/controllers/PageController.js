47ab8b82ae2423d4d309cbf4ac731401a099e13f
ObinShah <obin.shah@rediffmail.com>
2016-04-03 13:51:04 +0530

    Added files via upload

---
+++ Material Design/controllers/PageController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    angular.module('App')
+        .controller('PageController', ['$mdSidenav', pageController])
+        .controller('LeftController', function ($mdSidenav) {
+            var self = this;
+            self.close = function () {
+                $mdSidenav('left').close();
+            };
+        });
+
+    function pageController($mdSidenav) {
+        var self = this;
+
+        self.v = "variable";
+        self.toggleLeft = function () {
+            $mdSidenav('left').toggle();
+        };
+
+    }
+
+})();
